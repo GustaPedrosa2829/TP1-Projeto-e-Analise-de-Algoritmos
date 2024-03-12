d071fda68b9eaa91d4c203e8fa4ad7e27d61a98d
Gustavo Pedrosa <118071207+GustaPedrosa2829@users.noreply.github.com>
2024-03-12 20:42:18 -0300

    main.c

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "magicos.h"
+
+int main(int argc, char *argv[]) {
+    char *arquivoEntrada = NULL;
+    char *arquivoSaida = NULL;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "i:o:")) != -1) {
+        switch (opt) {
+            case 'i':
+                arquivoEntrada = optarg;
+                break;
+            case 'o':
+                arquivoSaida = optarg;
+                break;
+            default:
+                fprintf(stderr, "Uso: %s -i arquivo_entrada -o arquivo_saida\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (arquivoEntrada == NULL || arquivoSaida == NULL) {
+        fprintf(stderr, "Ambos os arquivos de entrada e saída são necessários.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    DadosEntrada dados;
+    lerEntrada(arquivoEntrada, &dados);
+
+    int *resultado = malloc(dados.k * sizeof(int));
+    processarRotas(&dados, resultado);
+    escreverSaida(arquivoSaida, resultado);
+
+    free(dados.voos);
+    free(resultado);
+
+    return 0;
+}
