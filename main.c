62d430fac886da7be74cc24815fe3bd031210c69
Gustavo Pedrosa <118071207+GustaPedrosa2829@users.noreply.github.com>
2024-04-06 12:48:20 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 #include "tp1.h"
 
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Uso: %s arquivo_entrada\n", argv[0]);
         return 1;
     }
 
-    struct rusage uso_inicio_cpu, uso_fim_cpu;
+    struct rusage inicio_cpu, fim_cpu;
     struct timeval inicio_clock, fim_clock;
 
     // Início da medição do tempo de CPU
-    getrusage(RUSAGE_SELF, &uso_inicio_cpu);
-
+    getrusage(RUSAGE_SELF, &inicio_cpu);
     // Início da medição do tempo de relógio
     gettimeofday(&inicio_clock, NULL);
 
@@ -38,6 +38,9 @@ int main(int argc, char *argv[]) {
 
     fclose(arquivo);
 
+    int caminhos = 0; // Inicializa o contador de caminhos
+    int visitado[MAX_N] = {0};
+
     // Executa o algoritmo de Dijkstra
     int distancias[MAX_N];
     dijkstra(&grafo, 0, distancias);
@@ -45,21 +48,27 @@ int main(int argc, char *argv[]) {
     // Ordena as distâncias
     qsort(distancias, n, sizeof(int), compara);
 
-    // Imprime as k menores distâncias
+    // Encontra os k caminhos mais curtos usando busca por profundidade
+    int caminhos_mais_curto[k];
+    dfs(&grafo, 0, n - 1, 0, visitado, &caminhos, k, distancias, caminhos_mais_curto);
+
+    // Ordena os caminhos mais curtos
+    qsort(caminhos_mais_curto, caminhos, sizeof(int), compara);
+
+    // Imprime os k caminhos mais curtos
     for (int i = 0; i < k; i++) {
-        printf("%d ", distancias[i]);
+        printf("%d ", caminhos_mais_curto[i]);
     }
     printf("\n");
 
     // Fim da medição do tempo de CPU
-    getrusage(RUSAGE_SELF, &uso_fim_cpu);
-
+    getrusage(RUSAGE_SELF, &fim_cpu);
     // Fim da medição do tempo de relógio
     gettimeofday(&fim_clock, NULL);
 
     // Cálculo dos tempos
-    double tempo_cpu = (uso_fim_cpu.ru_utime.tv_sec - uso_inicio_cpu.ru_utime.tv_sec) +
-                        (uso_fim_cpu.ru_utime.tv_usec - uso_inicio_cpu.ru_utime.tv_usec) / 1000000.0;
+    double tempo_cpu = (fim_cpu.ru_utime.tv_sec - inicio_cpu.ru_utime.tv_sec) +
+                        (fim_cpu.ru_utime.tv_usec - inicio_cpu.ru_utime.tv_usec) / 1000000.0;
 
     double tempo_clock = (fim_clock.tv_sec - inicio_clock.tv_sec) +
                         (fim_clock.tv_usec - inicio_clock.tv_usec) / 1000000.0;
