b161bd5d856606a03ec55f222697bfcf539c9655
Gustavo Pedrosa <118071207+GustaPedrosa2829@users.noreply.github.com>
2024-04-06 12:49:16 -0300

    Update tp1.h

--- tp1.h
+++ tp1.h
@@ -1,28 +1,27 @@
-
-#ifndef TP1_H
-#define TP1_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/resource.h>
 #include <sys/time.h>
+#include <limits.h>
 
 #define MAX_N 100005
 #define MAX_M 200005
 
-// Estrutura para representar uma aresta
+typedef struct {
+    int vertice;
+    int distancia;
+} NoFila;
+
 typedef struct {
     int destino;
     int custo;
 } Aresta;
 
-// Estrutura para representar um nó na lista de adjacência
 typedef struct {
     Aresta *arestas;
     int num_arestas;
 } Vertice;
 
-// Estrutura para representar o grafo
 typedef struct {
     Vertice vertices[MAX_N];
     int num_vertices;
@@ -31,8 +30,6 @@ typedef struct {
 void inicializarGrafo(Grafo *grafo, int num_vertices);
 void adicionarAresta(Grafo *grafo, int origem, int destino, int custo);
 void dijkstra(Grafo *grafo, int inicio, int *distancias);
+void dfs(Grafo *grafo, int vertice, int destino, int distancia, int *visitado, int *caminhos, int k, int *distancias, int *caminhos_mais_curto);
 int compara(const void *a, const void *b);
 void imprimirTempos(double tempo_cpu, double tempo_clock);
-
-#endif
-
